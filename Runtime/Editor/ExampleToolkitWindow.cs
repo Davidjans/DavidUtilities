76e4a327aaa383b535a450756be8fd288f9aebb0
David Janssen <davidjejans@hotmail.nl>
2025-05-02 12:45:10 +0200

    fixed

--- Runtime/Editor/ExampleToolkitWindow.cs
+++ Runtime/Editor/ExampleToolkitWindow.cs
@@ -10,6 +10,128 @@ public class ExampleToolWindow : EditorWindow
 
     private bool toggleState;
     private float sliderValue;
+    private StyleSheet currentStyleSheet;
+
+    [MenuItem("Window/Example Tool Window")]
+    public static void ShowWindow()
+    {
+        ExampleToolWindow window = GetWindow<ExampleToolWindow>("Example Tool");
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        if (data == null)
+        {
+            data = ScriptableObject.CreateInstance<ExampleData>();
+            toggleState = true;
+            sliderValue = 0.5f;
+            data.randomInt = Random.Range(0, 100);
+            data.randomString = "Hello World";
+            data.guidString = System.Guid.NewGuid().ToString();
+        }
+        dataEditor = Editor.CreateEditor(data);
+        BuildUI();
+    }
+
+    private void BuildUI()
+    {
+        VisualElement root = rootVisualElement;
+        root.Clear();
+        
+        ObjectField styleSheetField = ToolKitUtility.CreateObjectField("Load StyleSheet", typeof(StyleSheet), currentStyleSheet, null, root);
+        styleSheetField.RegisterValueChangedCallback(evt =>
+        {
+            if (evt.newValue is StyleSheet newSheet)
+            {
+                if (currentStyleSheet != null)
+                    root.styleSheets.Remove(currentStyleSheet);
+                currentStyleSheet = newSheet;
+                if (currentStyleSheet != null)
+                    root.styleSheets.Add(currentStyleSheet);
+            }
+        });
+        root.Add(styleSheetField);
+
+        
+        // popup for style sheeths
+        var styleOptions = new System.Collections.Generic.List<string>() { "Default", "Dark", "Light" ,"Cursed"};
+        PopupField<string> styleTemplatePopup = ToolKitUtility.CreatePopup(
+            "Style Template", styleOptions, "Default", (newTemplate) =>
+        {
+            string path = ToolKitUtility.GetStyleTemplatePath(newTemplate);
+            StyleSheet sheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(path);
+            if (sheet != null)
+            {
+                if (currentStyleSheet != null)
+                    root.styleSheets.Remove(currentStyleSheet);
+                currentStyleSheet = sheet;
+                root.styleSheets.Add(currentStyleSheet);
+            }
+        });
+        root.Add(styleTemplatePopup);
+
+        Button actionButton = null;
+
+        Toggle linkedtoggle = ToolKitUtility.CreateLinkedToggle(
+            "Linked toggle example",
+            () => data.linkedToggleExample,
+            (newValue) => { data.linkedToggleExample = newValue; },
+            () => Debug.Log("Linked toggle changed")
+        );
+        root.Add(linkedtoggle);
+
+        Toggle toggle = ToolKitUtility.CreateToggle("Enable Action Button", ref toggleState, () =>
+        {
+            if (actionButton != null)
+                actionButton.SetEnabled(toggleState);
+        });
+        root.Add(toggle);
+
+        actionButton = ToolKitUtility.CreateButton("Action Button", () =>
+        {
+            Debug.Log("Action button clicked!");
+        });
+        root.Add(actionButton);
+
+        Slider slider = ToolKitUtility.CreateSlider("Slider", 0f, 1f, sliderValue);
+        slider.RegisterValueChangedCallback(evt =>
+        {
+            sliderValue = evt.newValue;
+        });
+        root.Add(slider);
+
+        Box inlineEditorBox = ToolKitUtility.CreateBox("Inline Editor");
+        ObjectField dataField = ToolKitUtility.CreateObjectField("Select Data", typeof(ExampleData), data, dataEditor, root);
+        inlineEditorBox.Add(dataField);
+
+        var inlineInspector = ToolKitUtility.CreateInlineEditor(dataEditor);
+        inlineEditorBox.Add(inlineInspector);
+
+        root.Add(inlineEditorBox);
+    }
+    
+    private void OnDisable()
+    {
+        if (dataEditor != null)
+            DestroyImmediate(dataEditor);
+    }
+}
+
+
+/*
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+public class ExampleToolWindow : EditorWindow
+{
+    private ExampleData data;
+    private Editor dataEditor;
+
+    private bool toggleState;
+    private float sliderValue;
 
     [MenuItem("Window/Example Tool Window")]
     public static void ShowWindow()
@@ -41,6 +163,16 @@ public class ExampleToolWindow : EditorWindow
         
         Button actionButton = null;
 
+        // linked toggle example. This can be used to for example manage your settings or change settings between different tools.
+        Toggle linkedtoggle = ToolKitUtility.CreateLinkedToggle(
+            "Linked toggle example", 
+            () => data.linkedToggleExample, 
+            (newValue) => { data.linkedToggleExample = newValue; }, 
+            () => Debug.Log("Linked toggle changed")
+        );
+        root.Add(linkedtoggle);
+        
+        // normal toggle example
         Toggle toggle = ToolKitUtility.CreateToggle("Enable Action Button",ref toggleState, () =>
         {
             if (actionButton != null)
@@ -50,20 +182,25 @@ public class ExampleToolWindow : EditorWindow
         });
         root.Add(toggle);
 
+        // button example
         actionButton = ToolKitUtility.CreateButton("Action Button", () =>
         {
             Debug.Log("Action button clicked!");
         });
-        
         root.Add(actionButton);
 
+        // slider example
         Slider slider = ToolKitUtility.CreateSlider("Slider", 0f, 1f, sliderValue);
         root.Add(slider);
 
+        // how to put things in a box.
         Box inlineEditorBox = ToolKitUtility.CreateBox();
+        
+        // selection scriptableobjects or other unity object types.
         ObjectField dataField = ToolKitUtility.CreateScriptableField("Select Data", typeof(ExampleData), data, dataEditor, rootVisualElement);
         inlineEditorBox.Add(dataField);
         
+        //creating a inline editor for the selected object.
         IMGUIContainer inlineEditorContainer = new IMGUIContainer(() =>
         {
             if (dataEditor != null)
@@ -90,10 +227,44 @@ public class ExampleToolWindow : EditorWindow
     }
 }
 
+*/
+
 [CreateAssetMenu(fileName = "ExampleData", menuName = "Example/ExampleData")]
 public class ExampleData : ScriptableObject
 {
     public int randomInt;
     public string randomString;
     public string guidString;
+    public bool linkedToggleExample;
+    
 }
+/*#if UNITY_EDITOR
+[CustomEditor(typeof(ExampleData))]
+public class ExampleDataEditor : Editor
+{
+    public override VisualElement CreateInspectorGUI()
+    {
+        // root container for this inspector
+        var root = new VisualElement();
+
+        // debug header to verify we’re in UIElements land
+        var header = new Label("UIElements Inspector");
+        header.style.unityFontStyleAndWeight = FontStyle.Bold;
+        header.style.marginBottom = 4;
+        root.Add(header);
+
+        // bind to serializedObject so PropertyFields auto‑update
+        root.Bind(serializedObject);
+
+        // add each field explicitly
+        root.Add(new PropertyField(serializedObject.FindProperty("randomInt"),    "Random Int"));
+        root.Add(new PropertyField(serializedObject.FindProperty("randomString"), "Random String"));
+        root.Add(new PropertyField(serializedObject.FindProperty("guidString"),   "GUID"));
+        root.Add(new PropertyField(serializedObject.FindProperty("linkedToggleExample"),
+            "Linked Toggle"));
+
+        // return the fully built UIElements inspector
+        return root;
+    }
+}
+#endif*/
--- Runtime/Editor/Utility/ToolKitUtility.cs
+++ Runtime/Editor/Utility/ToolKitUtility.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -31,16 +32,37 @@ public static class ToolKitUtility
         });
         return slider;
     }
+    /// <summary>
+    /// The linked toggle will always change the bool it was originally given instead of creating a seperate variable.
+    /// </summary>
+    /// <param name="title"></param>
+    /// <param name="getValue"> () => boolName    To assign the link the variable</param>
+    /// <param name="setValue"> (newValue) => { boolName = newValue; }      To set the value of the linked variable</param>
+    /// <param name="toggleAction"></param>
+    /// <returns></returns>
+    public static Toggle CreateLinkedToggle(string title ,Func<bool> getValue, Action<bool> setValue, Action toggleAction)
+    {
+        Toggle toggle = new Toggle(title)
+        {
+            value = getValue()
+        };
+        toggle.RegisterValueChangedCallback(evt =>
+        {
+            setValue(evt.newValue);
+            toggleAction?.Invoke();
+        });
+        return toggle;
+    }
     
     public static Toggle CreateToggle(string title ,ref bool toggleValueVariable, Action toggleAction)
     {
-        Toggle toggle = new Toggle("Enable Action Button")
+        Toggle toggle = new Toggle(title)
         {
             value = toggleValueVariable
         };
         toggle.RegisterValueChangedCallback(evt =>
         {
-            toggleValueVariable = evt.newValue;
+            toggle.value = evt.newValue;
             toggleAction?.Invoke();
         });
         return toggle;
@@ -67,7 +89,7 @@ public static class ToolKitUtility
     /// <param name="dataEditor">Technically not necesarry but will cause issues with redo and undo if not present</param>
     /// <param name="rootVisualElement"> Technically not necesarry but will cause issues with redo and undo if not present</param>
     /// <returns></returns>
-    public static ObjectField CreateScriptableField(string title = "Select Object", System.Type objectType = null, Object defaultValue = null, Editor dataEditor = null, VisualElement rootVisualElement = null )
+    public static ObjectField CreateObjectField(string title = "Select Object", System.Type objectType = null, Object defaultValue = null, Editor dataEditor = null, VisualElement rootVisualElement = null )
     {
         ObjectField objectField = new ObjectField(title)
         {
@@ -76,22 +98,21 @@ public static class ToolKitUtility
         };
         objectField.RegisterValueChangedCallback(evt =>
         {
-            if (evt.newValue is ExampleData newData)
+            Undo.RecordObject(evt.newValue, "Select New ExampleData");
+            objectField.value = evt.newValue;
+            
+            if (dataEditor != null)
             {
-                Undo.RecordObject(newData, "Select New ExampleData");
-                objectField.value = newData;
-
-                if (objectField.value == null)
-                {
-                    return;
-                }
-                if (dataEditor != null)
-                {
-                    MonoBehaviour.DestroyImmediate(dataEditor);
-                }
-                dataEditor = Editor.CreateEditor(objectField.value);
-                if (rootVisualElement != null) rootVisualElement.MarkDirtyRepaint();
+                MonoBehaviour.DestroyImmediate(dataEditor);
             }
+            
+            if (objectField.value == null)
+            {
+                return;
+            }
+           
+            dataEditor = Editor.CreateEditor(objectField.value);
+            if (rootVisualElement != null) rootVisualElement.MarkDirtyRepaint();
         });
         
         objectField.RegisterValueChangedCallback(evt =>
@@ -103,5 +124,41 @@ public static class ToolKitUtility
         });
         return objectField;
     }
+    public static PopupField<string> CreatePopup(string title, List<string> options, string defaultValue, Action<string> onValueChanged)
+    {
+        PopupField<string> popup = new PopupField<string>(title, options, defaultValue);
+        popup.RegisterValueChangedCallback(evt =>
+        {
+            onValueChanged?.Invoke(evt.newValue);
+        });
+        return popup;
+    }
+
+    public static string GetStyleTemplatePath(string templateName)
+    {
+        switch (templateName)
+        {
+            case "Dark":
+                return "Assets/DavidUtilities/Runtime/Styles/Dark.uss";
+            case "Light":
+                return "Assets/DavidUtilities/Runtime/Styles/Light.uss";
+            case "Cursed":
+                return "Assets/DavidUtilities/Runtime/Styles/Cursed.uss";
+            default:
+                return "Assets/DavidUtilities/Runtime/Styles/Default.uss";
+        }
+    }
     
+    /// <summary>
+    /// Creates a fully‑automated UIElements inspector for the given Editor.
+    /// The returned VisualElement is bound to editor.serializedObject and
+    /// will pick up any USS styles applied to its parent.
+    /// </summary>
+    public static VisualElement CreateInlineEditor(Editor editor)
+    {
+        var container = new VisualElement();
+        container.Bind(editor.serializedObject);
+        InspectorElement.FillDefaultInspector(container, editor.serializedObject, editor);
+        return container;
+    }
 }
